436a0c49519ba464b9333234eb7db3d436939db3
ckstmznf <ckstmznf@naver.com>
2021-07-31 22:26:36 +0900

    SingUp

--- app/src/main/java/com/example/digital_contest/LoginActivity.kt
+++ app/src/main/java/com/example/digital_contest/LoginActivity.kt
@@ -1,11 +1,34 @@
 package com.example.digital_contest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        //matching
+        val inputId = findViewById<EditText>(R.id.edt_login_inputId)
+        val inputPassword = findViewById<EditText>(R.id.edt_login_inputPassword)
+        val loginBtn = findViewById<Button>(R.id.btn_login_login)
+        val singUp = findViewById<TextView>(R.id.txt_login_singUp)
+
+        singUp.setOnClickListener{
+            val intent = Intent(this, SingUpActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/digital_contest/SingUpActivity.kt
+++ app/src/main/java/com/example/digital_contest/SingUpActivity.kt
@@ -1,11 +1,66 @@
 package com.example.digital_contest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SingUpActivity : AppCompatActivity() {
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+        auth = FirebaseAuth.getInstance()
+
+        //matching
+        val inputId = findViewById<EditText>(R.id.edt_singUp_inputId)
+        val inputName = findViewById<EditText>(R.id.edt_singUp_inputName)
+        val inputEmail = findViewById<EditText>(R.id.edt_singUp_inputEmail)
+        val inputPassword = findViewById<EditText>(R.id.edt_singUp_inputPassword)
+        val inputPasswordRe = findViewById<EditText>(R.id.edt_singUp_inputPasswordRe)
+        val singUpBtn = findViewById<Button>(R.id.btn_singUp_singUp)
+
+
+        singUpBtn.setOnClickListener{
+            if(TextUtils.isEmpty(inputId.text.toString())){ // inputId.text.toString()이 null인지 판단
+                inputId.setError("ID를 입력헤주세요.")
+                return@setOnClickListener//null이라면 에러 반환
+
+            }   else if(TextUtils.isEmpty((inputEmail.text.toString()))){
+                inputEmail.setError("이메일을 입력헤주세요.")
+                return@setOnClickListener
+
+            }   else if(TextUtils.isEmpty((inputPassword.text.toString()))){
+                inputPassword.setError("비밀번호를 입력헤주세요.")
+                return@setOnClickListener
+
+            }   else if(TextUtils.isEmpty((inputPasswordRe.text.toString()))){
+                inputPasswordRe.setError("비밀번호를 다시 입력헤주세요.")
+                return@setOnClickListener
+
+            }   else if(inputPassword.text.toString() != inputPasswordRe.text.toString()){
+                inputPassword.setError("비밀번호가 일치하지 않습니다.")
+                inputPasswordRe.setError("비밀번호가 일치하지 않습니다.")
+                return@setOnClickListener
+
+            }
+
+
+
+            auth.createUserWithEmailAndPassword(inputEmail.text.toString(), inputPassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "회원가입에 성공하였습니다.", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }   else{
+                        Toast.makeText(this, "회원가입에 실패하였습니다.", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
 }
