69c2268d5c502c4bf89885dd1dc9e271d28f758f
ckstmznf <ckstmznf@naver.com>
2021-07-31 23:00:43 +0900

    Login

--- app/src/main/java/com/example/digital_contest/LoginActivity.kt
+++ app/src/main/java/com/example/digital_contest/LoginActivity.kt
@@ -3,9 +3,12 @@ package com.example.digital_contest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.TextureView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
@@ -28,7 +31,28 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        loginBtn.setOnClickListener{
+            if(TextUtils.isEmpty(inputId.text.toString())){
+                inputId.setError("ID를 입력해주세요.")
+                return@setOnClickListener
+            }   else if(TextUtils.isEmpty(inputPassword.text.toString())){
+                inputPassword.setError("비밀번호를 입력해주세요")
+                return@setOnClickListener
+            }
 
+            auth.signInWithEmailAndPassword(inputId.text.toString(), inputPassword.text.toString())
+                .addOnCompleteListener(this){task ->
+                    if(task.isSuccessful){
+                        //로그인에 성공했을때
+                        Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }   else{
+                        //로그인에 실패했을때
+                        Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
 
     }
 }
