4e24e8cf052629eb0df0a6eafbebbfe6a119e02a
Hwls_ek <nunnunan0321@naver.com>
2021-08-03 17:44:08 +0900

    보이니...히찬아.......?

--- app/src/main/java/com/example/digital_contest/MainActivity.kt
+++ app/src/main/java/com/example/digital_contest/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //보이니.......?.....커밋이....? ... .
     }
 }
