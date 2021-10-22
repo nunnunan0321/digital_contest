d6e37d93849c2c8a1b04b0b5778a3db76b240411
장인수 <insu12345_@naver.com>
2021-10-23 02:21:47 +0900

    지도 구현 GPS 구현 완료 에러 없음

--- app/src/main/java/com/example/digital_contest/Activity/Main/MainActivity.kt
+++ app/src/main/java/com/example/digital_contest/Activity/Main/MainActivity.kt
@@ -1,22 +1,45 @@
 package com.example.digital_contest.Activity.Main
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.viewpager.widget.ViewPager
 import com.example.digital_contest.Activity.Main.Fragment.FragmentAdapter
 import com.example.digital_contest.R
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d(TAG, "정확한 위치 권한 허용됨")
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d(TAG, "대략적인 위치 권한 허용됨")
+                } else -> {
+                Log.d(TAG, "위치 권한 거부됨")
+                Toast.makeText(this, "위치 권한이 거부되었습니다. 앱을 재실행해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
 
 
         val pagerAdapter = FragmentAdapter(supportFragmentManager)
@@ -28,6 +51,4 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
 }
-
