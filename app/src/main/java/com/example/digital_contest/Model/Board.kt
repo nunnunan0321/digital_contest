95b3ff6202ed7eee15cf010a673dd121c9e35e6b
ckstmznf <ckstmznf@naver.com>
2021-10-23 21:47:47 +0900

    에러 고침용 커밋

--- app/src/main/java/com/example/digital_contest/Model/Board.kt
+++ app/src/main/java/com/example/digital_contest/Model/Board.kt
@@ -7,6 +7,5 @@ data class Board(
     val writerID : String, //작성자 ID
     val contents : String, //게시물의 내용
     val lickCount : Int, //게시물의 좋아요 수
-
 //    val position  : GeoPoint //위치정보를 저장
 ) : Serializable
