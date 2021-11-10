eeb98840bb5960427b2445adb61696d011916883
장인수 <insu12345_@naver.com>
2021-11-11 00:50:12 +0900

    구글맵 완전구현완료됨, 위치 구하기, 카메라 이동, 에러 없음

--- app/src/main/java/com/example/digital_contest/activity/main/fragment/MainFragment.kt
+++ app/src/main/java/com/example/digital_contest/activity/main/fragment/MainFragment.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +20,7 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.digital_contest.R
+import com.google.android.gms.location.*
 import com.example.digital_contest.activity.main.MainActivity
 import com.example.digital_contest.activity.write.WriteActivity
 import com.example.digital_contest.databinding.FragmentMainTabBinding
@@ -40,6 +42,10 @@ class MainFragment:Fragment(),
     lateinit var binding : FragmentMainTabBinding
     private var permissionDenied = false
     private lateinit var map: GoogleMap
+    lateinit var location : Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var locationCallbackCheck : Boolean = true
 
 
     override fun onCreateView(
@@ -66,11 +72,13 @@ class MainFragment:Fragment(),
         return frag
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         class MainActivity : AppCompatActivity()
 
         //getAddress(LatLng(37.5759396,126.9758432))
+
 
     }
 
@@ -84,26 +92,74 @@ class MainFragment:Fragment(),
         }
     }
 
+//    override fun onPause() {
+//        super.onPause()
+//        stopLocationUpdates()
+//    }
+
+    // 지도가 준비되었을 때 실행되는 함수
+    @SuppressLint("MissingPermission") // Permission 컴파일 에러 제거
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap ?: return
         googleMap.setOnMyLocationButtonClickListener(this)
         googleMap.setOnMyLocationClickListener(this)
         enableMyLocation()
         googleMap.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+
+        fusedLocationClient =  LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 20 * 1000
+
+        // location 콜백
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult == null) {
+                    return
+                }
+                for (location in locationResult.locations) {
+                    if (location != null) {
+                        // 위치
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+
+                        Log.d("Test", "GPS Location changed, Latitude: $latitude" +
+                                ", Longitude: $longitude")
+
+                        // locationCallback 이 최초 1회 실행되었을 때만
+                        if(locationCallbackCheck) {
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 14.0F))
+                        }
+                    }
+                }
+                locationCallbackCheck = false
+            }
+        }
+
+
+        // location 업데이트 요청, 업데이트 시작
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            Looper.getMainLooper());
     }
 
+    // 지도 내에서 내 위치 버튼을 클릭했을 때 실행되는 함수
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(requireContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show()
         // 카메라가 현재 위치로 이동됨
         return false
     }
 
+    // 지도 내에서 현재 위치를 클릭했을 때(파란색) 실행되는 함수
     override fun onMyLocationClick(location: Location) {
         //location 에 위치 정보
         Toast.makeText(requireContext(), "Current location:\n$location", Toast.LENGTH_LONG).show()
         Log.d(TAG, "location : ${location.latitude} ${location.longitude}")
+        //map.addMarker(MarkerOptions().position(LatLng(10.0, 0.0)).title("Marker"))
     }
 
+    // Permisson 에 대한 결과를 반환하는 함수
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -113,7 +169,7 @@ class MainFragment:Fragment(),
 
     }
 
-
+    // 맵이 켜지지 않았을 때 실행되는 함수
     private fun setUpMapIfNeeded() {
         val mapFragment : SupportMapFragment = SupportMapFragment.newInstance()
         childFragmentManager.beginTransaction().replace(R.id.map, mapFragment, "MapTag").commit()
@@ -123,6 +179,7 @@ class MainFragment:Fragment(),
     @SuppressLint("MissingPermission")
     private fun enableMyLocation() {
         if (!::map.isInitialized) return
+        // Permission Check
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
             map.isMyLocationEnabled = true
@@ -130,9 +187,15 @@ class MainFragment:Fragment(),
         }
     }
 
+    // Permission 이 제대로 들어오지 않았을 때 실행되는 함수
     private fun showMissingPermissionError() {
 
     }
+
+    // 현재 위치 정보 업데이트 중지
+//    private fun stopLocationUpdates() {
+//        fusedLocationClient.removeLocationUpdates(locationCallback)
+//    }
 
     private fun getAddress(position: LatLng) {
         val geoCoder = Geocoder(context, Locale.getDefault())
@@ -142,7 +205,6 @@ class MainFragment:Fragment(),
 
         Log.e("Address", address)
     }
-
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
