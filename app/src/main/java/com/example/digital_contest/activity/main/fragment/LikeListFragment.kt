97ae2436c0854e3be3834a749cf105b18537cc58
장인수 <insu12345_@naver.com>
2021-11-17 23:08:54 +0900

    좋아요 목록 수정

--- app/src/main/java/com/example/digital_contest/activity/main/fragment/LikeListFragment.kt
+++ app/src/main/java/com/example/digital_contest/activity/main/fragment/LikeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.digital_contest.R
@@ -33,8 +34,11 @@ class LikeListFragment:Fragment(){
             val likeList = boardDB.getLikeBoards((activity as MainActivity).userData.likeBoardList)
             Log.d("likeList", likeList.toString())
             val likeListAdapter = LikeListAdapter(likeList)
+            likeListAdapter.notifyDataSetChanged()
+
             withContext(Dispatchers.Main){
                 binding.recyclerLikeList.adapter = likeListAdapter
+
             }
         }
 
