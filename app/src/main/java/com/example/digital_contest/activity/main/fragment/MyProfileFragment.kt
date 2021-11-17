0b4601f42991a8f1f32470c5a93a9ed5b28c80b1
ckstmznf <ckstmznf@naver.com>
2021-11-18 01:23:55 +0900

    Profile

--- app/src/main/java/com/example/digital_contest/activity/main/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/example/digital_contest/activity/main/fragment/MyProfileFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.digital_contest.R
+import com.example.digital_contest.activity.main.MainActivity
+import com.example.digital_contest.databinding.FragmentMyProfileBinding
 
 class MyProfileFragment : Fragment() {
+    lateinit var binding : FragmentMyProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile, container, false)
+        val root = binding.root
+
+        binding.userData = (activity as MainActivity).userData
+
+        return root
     }
 }
